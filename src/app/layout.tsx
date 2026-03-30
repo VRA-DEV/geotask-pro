ae92c3dfbe2fbc7ddb7988b19299daba91b2d214
dev-vra <dev.vinnreis@gmail.com>
2026-03-30 08:45:09 -0400

    fix: chrome translate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" translate="no">
       <body
         className={`${inter.variable} ${jakarta.variable} ${jetbrains.variable} font-sans antialiased`}
       >
